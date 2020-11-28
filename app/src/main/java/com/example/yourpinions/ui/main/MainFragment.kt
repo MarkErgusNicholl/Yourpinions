6bd4610c2770da4ba4ca9fa964c144d0bab035ab
Mark <mn16660@my.bristol.ac.uk>
2020-11-28 11:18:51 +0800

    Simple recycler view working

--- app/src/main/java/com/example/yourpinions/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/yourpinions/ui/main/MainFragment.kt
@@ -2,11 +2,16 @@ package com.example.yourpinions.ui.main
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.yourpinions.MainActivity
 import com.example.yourpinions.R
+import com.example.yourpinions.data.Yourpinion
 
 class MainFragment : Fragment() {
 
@@ -15,10 +20,75 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var adapter: YourpinionRecyclerViewAdapter
+
+    // DUMMY DATA
+    private val dummyYourpinionList = ArrayList<Yourpinion>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
+        dummyYourpinionList.add(Yourpinion("This"))
+        dummyYourpinionList.add(Yourpinion("App"))
+        dummyYourpinionList.add(Yourpinion("Is"))
+        dummyYourpinionList.add(Yourpinion("Kinda"))
+        dummyYourpinionList.add(Yourpinion("Sus"))
+        dummyYourpinionList.add(Yourpinion("This"))
+        dummyYourpinionList.add(Yourpinion("App"))
+        dummyYourpinionList.add(Yourpinion("Is"))
+        dummyYourpinionList.add(Yourpinion("Kinda"))
+        dummyYourpinionList.add(Yourpinion("Sus"))
+        dummyYourpinionList.add(Yourpinion("This"))
+        dummyYourpinionList.add(Yourpinion("App"))
+        dummyYourpinionList.add(Yourpinion("Is"))
+        dummyYourpinionList.add(Yourpinion("Kinda"))
+        dummyYourpinionList.add(Yourpinion("Sus"))
+        dummyYourpinionList.add(Yourpinion("This"))
+        dummyYourpinionList.add(Yourpinion("App"))
+        dummyYourpinionList.add(Yourpinion("Is"))
+        dummyYourpinionList.add(Yourpinion("Kinda"))
+        dummyYourpinionList.add(Yourpinion("Sus"))
+        dummyYourpinionList.add(Yourpinion("This"))
+        dummyYourpinionList.add(Yourpinion("App"))
+        dummyYourpinionList.add(Yourpinion("Is"))
+        dummyYourpinionList.add(Yourpinion("Kinda"))
+        dummyYourpinionList.add(Yourpinion("Sus"))
+        dummyYourpinionList.add(Yourpinion("This"))
+        dummyYourpinionList.add(Yourpinion("App"))
+        dummyYourpinionList.add(Yourpinion("Is"))
+        dummyYourpinionList.add(Yourpinion("Kinda"))
+        dummyYourpinionList.add(Yourpinion("Sus"))
+        dummyYourpinionList.add(Yourpinion("This"))
+        dummyYourpinionList.add(Yourpinion("App"))
+        dummyYourpinionList.add(Yourpinion("Is"))
+        dummyYourpinionList.add(Yourpinion("Kinda"))
+        dummyYourpinionList.add(Yourpinion("Sus"))
+        dummyYourpinionList.add(Yourpinion("This"))
+        dummyYourpinionList.add(Yourpinion("App"))
+        dummyYourpinionList.add(Yourpinion("Is"))
+        dummyYourpinionList.add(Yourpinion("Kinda"))
+        dummyYourpinionList.add(Yourpinion("Sus"))
+        dummyYourpinionList.add(Yourpinion("This"))
+        dummyYourpinionList.add(Yourpinion("App"))
+        dummyYourpinionList.add(Yourpinion("Is"))
+        dummyYourpinionList.add(Yourpinion("Kinda"))
+        dummyYourpinionList.add(Yourpinion("Sus"))
+        dummyYourpinionList.add(Yourpinion("This"))
+        dummyYourpinionList.add(Yourpinion("App"))
+        dummyYourpinionList.add(Yourpinion("Is"))
+        dummyYourpinionList.add(Yourpinion("Kinda"))
+        dummyYourpinionList.add(Yourpinion("Sus"))
         return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView = view.findViewById(R.id.mainRecyclerView)
+        linearLayoutManager = LinearLayoutManager(activity)
+        adapter = YourpinionRecyclerViewAdapter(dummyYourpinionList)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = linearLayoutManager
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/yourpinions/ui/main/YourpinionRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/yourpinions/ui/main/YourpinionRecyclerViewAdapter.kt
@@ -1,38 +1,58 @@
 package com.example.yourpinions.ui.main
 
 import android.app.LauncherActivity
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.yourpinions.R
+import com.example.yourpinions.data.Yourpinion
+import com.example.yourpinions.inflate
 
-class YourpinionRecyclerViewAdapter() : RecyclerView.Adapter<YourpinionRecyclerViewAdapter.ViewHolder>() {
+class YourpinionRecyclerViewAdapter(private val yourpinionsList: ArrayList<Yourpinion>)
+    : RecyclerView.Adapter<YourpinionRecyclerViewAdapter.YourpinionHolder>() {
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val yourpinionText: TextView
-        val voteCountText: TextView
+    class YourpinionHolder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener {
+        private var view: View = v
+        private var yourpinion: Yourpinion? = null
+        var opinion: TextView
+        var voteCount: TextView
 
         init {
-            // Define click listener for the ViewHolder's View.
-            yourpinionText = view.findViewById(R.id.yourpinion)
-            voteCountText = view.findViewById(R.id.votecount)
+            v.setOnClickListener(this)
+            opinion = view.findViewById(R.id.yourpinion)
+            voteCount = view.findViewById(R.id.votecount)
         }
-    }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): YourpinionRecyclerViewAdapter.ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.recyclerview_yourpinion_item, parent, false)
+        override fun onClick(v: View) {
+            Log.d("RecyclerView", "CLICK!")
+        }
 
-        return ViewHolder(view)
+        fun bindYourpinion(yourpinion: Yourpinion) {
+            this.yourpinion = yourpinion
+            voteCount.text = yourpinion.vote_count.toString()
+            opinion.text = yourpinion.opinion
+        }
+
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return yourpinionsList.count()
     }
 
-    override fun onBindViewHolder(holder: YourpinionRecyclerViewAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): YourpinionHolder {
+        val inflatedView = parent.inflate(R.layout.recyclerview_yourpinion_item, false)
+        return YourpinionHolder(inflatedView)
+    }
+
+    override fun onBindViewHolder(holder: YourpinionHolder, position: Int) {
+        val itemYourpinion = yourpinionsList[position]
+        Log.d("RecyclerView", "onBindViewHolder")
+        holder.bindYourpinion(itemYourpinion)
     }
 }
