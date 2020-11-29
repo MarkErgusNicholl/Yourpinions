c69cc683a5f3ac40b1fefc7aeb2442f5ef11b93a
Mark <mn16660@my.bristol.ac.uk>
2020-11-29 22:37:23 +0800

    Unfinished testing: Cannot get Mockito inline to mock final classes

--- app/src/test/java/com/example/yourpinions/YourpinionUnitTest.kt
+++ app/src/test/java/com/example/yourpinions/YourpinionUnitTest.kt
@@ -5,9 +5,37 @@ import org.junit.Assert
 import org.junit.Test
 
 class YourpinionUnitTest {
+    private val testOpinion = "Test"
+    private val testUid = "111000333444"
+    private val testVoteCount = 888
+
     @Test
     fun voteCount_InitAsZero() {
-        val testOpinion : Yourpinion = Yourpinion("Test")
-        Assert.assertEquals(0, testOpinion.vote_count)
+        val testYourpinion = Yourpinion(testOpinion)
+        Assert.assertEquals(0, testYourpinion.vote_count)
+    }
+
+    @Test
+    fun uid_isNullableOnInit() {
+        val testYourpinion = Yourpinion(testOpinion, testVoteCount)
+        Assert.assertEquals(null, testYourpinion.uid)
+    }
+
+    @Test
+    fun uid_initProperly() {
+        val testYourpinion = Yourpinion(testOpinion, testVoteCount, testUid)
+        Assert.assertEquals(testUid, testYourpinion.uid)
+    }
+
+    @Test
+    fun opinion_initProperly() {
+        val testYourpinion = Yourpinion(testOpinion, testVoteCount, testUid)
+        Assert.assertEquals(testOpinion, testYourpinion.opinion)
+    }
+
+    @Test
+    fun voteCount_initProperly() {
+        val testYourpinion = Yourpinion(testOpinion, testVoteCount, testUid)
+        Assert.assertEquals(testVoteCount, testYourpinion.vote_count)
     }
 }
